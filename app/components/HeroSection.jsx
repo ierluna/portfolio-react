edb195bdef3b5b4516807020ce68053c4af67efc
ierluna <lunjavag@gmail.com>
2024-07-10 15:47:57 -0300

    added title with media queries

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -2,15 +2,19 @@
 import React from "react";
 
 const HeroSection = () => {
-    return (
-        <section>
-            <h1 className=" text-white ">
-                Hello! I'm Javier
-            </h1>
-        </section>
-    );
-
+  return (
+    <section>
+      <h1 className=" text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+        Hello! I'm Javier
+      </h1>
+      <p className="text-[#ADB7BE] text-lg lg:text-xl">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+        quas magni reprehenderit mollitia? Fuga officiis quasi tempora natus est
+        culpa necessitatibus tempore. Iste, incidunt sequi ad autem porro harum
+        veritatis.
+      </p>
+    </section>
+  );
 };
-
 
 export default HeroSection;
