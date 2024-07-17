f37ab2f4318eb92a2ced67f084700116e7d13cac
ierluna <lunjavag@gmail.com>
2024-07-17 14:43:17 -0300

    fixed border radius of image

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -17,15 +17,15 @@ const HeroSection = () => {
             porro harum veritatis.
           </p>
         </div>
-        <div className="col-span-5 place-self-center  mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative ">
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="relative bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] flex items-center justify-center">
             <Image
               src="/images/developerStockImage.jpg"
-              alt="a person staring at two computer screens developing software"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 "
-              width={300}
-              height={300}
-            ></Image>
+              alt="avatar of a developer"
+              width={400}
+              height={400}
+              className=" rounded-full"
+            />
           </div>
         </div>
       </div>
